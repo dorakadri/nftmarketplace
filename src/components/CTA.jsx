e6365d7268ab4df3cc6ed7fb5c06267ca45fbade
dorakadri <dora.kadri@esprit.tn>
2023-06-23 22:11:25 +0100

    finalebuilded

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -6,7 +6,7 @@ const CTA = () =>
   (
     <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding}  sm:flex-row flex-col outline-gradient2  box-shadow `}>
       <div className="flex-1 flex flex-col  ">
-        <h2 className={styles.heading2} >LET try our service now!</h2>
+        <h2 className={styles.heading2} >Join the Digital Art Revolution now!</h2>
         <p className={`${styles.paragraph} `}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, temporibus eius maxime voluptates aspernatur saepe dignissimos delectus?
         </p>
       </div>
